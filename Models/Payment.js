452be90692dc9661161b20a14f65277f478d30a0
Habib <hanbli.habib@gmail.com>
2024-08-07 16:01:38 +0200

    création du fichier 'Payment.js'

---
+++ Models/Payment.js
@@ -0,0 +1,40 @@
+const {Model, DataTypes} = require('sequelize');
+
+const sequelize = require('./Config/Sequelize');
+
+class Payment extends Model{}
+
+Payment.init({
+    pay_id:{
+        type: DataTypes.INTEGER,
+        primaryKey: true,
+        autoIncrement: true
+    },
+    or_id:{
+        type: DataTypes.INTEGER,
+        allowNull: false
+    },
+    payment_method:{
+        type: DataTypes.STRING,
+        allowNull: false
+    },   
+    payment_amount:{
+        type: DataTypes.DECIMAL(10,2)
+    }, 
+    payment_date:{
+        type: DataTypes.DATE,
+        allowNull: false
+    },
+    status:{
+        type: DataTypes.ENUM('pending', 'success', 'failed'),
+        allowNull: false
+    }
+
+},{
+    sequelize,
+    modelName:"Payment",
+    tableName:"payments",
+    timestamps:true 
+});
+
+module.exports = Payment;
