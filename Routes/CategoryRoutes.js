7f8b0efafd1271e0cdd149db6e3797782768c104
Habib7150 <hanbli.habib@gmail.com>
2024-08-13 12:09:15 +0200

    création du fichier 'CategoryRoutes.js'

---
+++ Routes/CategoryRoutes.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const router = express.Router();
+const CategotiyController = require('../Controllers/CategoryController');
+
+// Get all categories
+
+router.get("/",(request,response) => {CategotiyController.getAllCategories(request,response);});
+
+// Get single category
+
+router.get("/:id",(request,response) => {CategotiyController.getCategoryById(request,response);});
+
+// Add new category
+
+router.post("/",(request,response) => {CategotiyController.addCategory(request,response);});
+
+// Update a category
+
+router.put("/:id",(request,response) => {CategotiyController.updateCategory(request,response);});
+
+// Delete a category
+
+router.delete("/:id",(request,response) => {CategotiyController.deleteCategory(request,response);});
+
+module.exports = router;
+
