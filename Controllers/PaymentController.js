ffc646707ae447f04fe5e97236bd8b7ca2325697
Habib7150 <hanbli.habib@gmail.com>
2024-08-15 12:38:20 +0200

    création du fichier PaymentController

---
+++ Controllers/PaymentController.js
@@ -0,0 +1,33 @@
+const PaymentService = require('./Services/PaymentService');
+
+class PaymentController {
+    async getAllPayments(request, response) {
+        try {
+            const payments = await PaymentService.getAllPayments();
+            response.json(payments);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+    async getPaymentById(request, response) {
+        try {
+            const payment = await PaymentService.getPaymentById(request.params.id);
+            response.json(payment);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+    async addPayment(request, response) {
+        try {
+            const payment = await PaymentService.addPayment(request.body);
+            response.json(payment);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+}
+
+module.exports = new PaymentController();
+
+
+
