4fc9c3d262cc46387f0b6a538561d73d31462f21
Habib7150 <hanbli.habib@gmail.com>
2024-08-15 13:08:19 +0200

    création du fichier ReviewController

---
+++ Controllers/ReviewController.js
@@ -0,0 +1,48 @@
+const ReviewService = require('./Services/ReviewService');
+
+class ReviewController {
+    async getAllReviews(request, response) {
+        try {
+            const reviews = await ReviewService.getAllReviews();
+            response.json(reviews);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+    async getReviewById(request, response) {
+        try {
+            const review = await ReviewService.getReviewById(request.params.id);
+            response.json(review);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+    async addReview(request, response) {
+        try {
+            const review = await ReviewService.addReview(request.body);
+            response.json(review);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+
+    async updateReview(request, response) {
+        try {
+            const review = await ReviewService.updateReview(request.params.id, request.body);
+            response.json(review);
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+
+    async deleteReview(request, response) {
+        try {
+            await ReviewService.deleteReview(request.params.id);
+            response.status(204).send();
+        } catch (error) {
+            response.status(500).json({ error: error.message });
+        }
+    }
+}
+
+module.exports = new ReviewController();
