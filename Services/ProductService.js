576270a2eefa65b01ef9282aba6b28331541fe07
Habib7150 <hanbli.habib@gmail.com>
2024-08-16 11:38:39 +0200

    création du ProductService

--- Services/ProductService.js
+++ Services/ProductService.js
@@ -2,6 +2,22 @@ const Product = require('../Models/Product');
 
 class ProductService {
     async getAllProducts(){
-        return await Product.findAll({include: ["category"]});
+        return await Product.findAll({include: ['Category']});
+    }
+
+    async getProductById(productId){
+        return await Product.findByPk(productId, {include: ['Category']});
+    }
+
+    async addProduct(productData){
+        return await Product.create(productData);
+    }
+
+    async updateProduct(productId, productData){
+        const product = await Product.findByPk(productId);
+        if(!product) throw new Error('Product not found');
+        return await product.update(productData);
     }
 }
+
+module.exports = new ProductService();
