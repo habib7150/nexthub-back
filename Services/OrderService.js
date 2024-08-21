6c3cdff4df6e2ff0ab57ff0389565a06855f908f
Habib7150 <hanbli.habib@gmail.com>
2024-08-21 11:55:39 +0200

    modification de la ligne 5 et la ligne 9 dans 'OrderService'

--- Services/OrderService.js
+++ Services/OrderService.js
@@ -2,11 +2,11 @@ const Order = require('../Models/Order');
 
 class OrderService {
     async getAllOrders(){
-        return await Order.findAll({include:['user', 'product' ]});
+        return await Order.findAll({include:['user']});
     }
 
     async getOrderById(orderId){
-        return await Order.findByPk(orderId,{include:['user', 'product' ]});
+        return await Order.findByPk(orderId,{include:['user']});
     }
 
     async addOrder(orderData){
